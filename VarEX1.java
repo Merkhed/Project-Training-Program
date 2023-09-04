0d97309f97f0868ebede2125cc51ae8fd02e007e
Merkhed <144015301+Merkhed@users.noreply.github.com>
2023-09-04 16:55:54 +0530

    Create VarEX1.java
    New file added

---
+++ VarEX1.java
@@ -0,0 +1,16 @@
+class Helloworld
+{
+    public static void main (string []args)
+    {
+        //create variable
+        //Data type var-name
+        int rollNo;
+        rollNo=10;
+        int age =21;
+        double price=105.55;
+        boolean isfree=true;
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isfree);
+    }
+}
